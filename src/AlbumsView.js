5edb788b9c77b55ab8a17a9cf899d8f1fa5ded19
Nikita Andreiev <nickandreev47@gmail.com>
2018-03-01 22:52:43 +0300

    update

--- src/AlbumsView.js
+++ src/AlbumsView.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 class AlbumsView extends Component {
     render() {
         return (
             <div className = "AlbumsView nav-bar">
-                <button onclick={() => {
+                <button onClick={() => {
                     
+                    // window.location = "https://www.instagram.com/oauth/authorize/?client_id=f63560f0053148a4b4c4b583bdc338ea&redirect_uri=https://uran-project.herokuapp.com/&response_type=token";
+                    axios.get("https://www.instagram.com/oauth/authorize/?client_id=f63560f0053148a4b4c4b583bdc338ea&redirect_uri=https://uran-project.herokuapp.com/&response_type=token").then(function(response){
+                        console.log(response);
+                     
+                        // window.onload(function () {
+                        //     var accessToken = window.location.href;
+                        //     console.log(accessToken);
+                        //     console.log("loaded");
+                        // });
+                    });
+
                 }}>Login or Sign Up to Instagram</button>
             </div>
         );
--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar.js';
+import AlbumsView from './AlbumsView.js';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<NavBar />
-
+				<AlbumsView />
 
 			</div>
 		);
