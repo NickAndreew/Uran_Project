84b8d9ab19bb868f05db5ac01b8a8859a985d4bf
Nikita Andreiev <nickandreev47@gmail.com>
2018-03-01 18:00:34 +0300

    uran project

---
+++ src/AlbumsView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class AlbumsView extends Component {
+    render() {
+        return (
+            <div className = "AlbumsView nav-bar">
+                <button onclick={() => {
+                    
+                }}>Login or Sign Up to Instagram</button>
+            </div>
+        );
+    }
+}
+
+export default AlbumsView;
---
+++ src/NavBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+class NavBar extends Component {
+	render() {
+		return (
+			<div className="NavBar">
+				<nav className="navbar navbar-inverse">
+					<div className="container-fluid">
+						<div className="navbar-header">
+							<a className="navbar-brand" href="#">WebSiteName</a>
+						</div>
+						<ul className="nav navbar-nav">
+							<li className="active"><a href="#">Home</a></li>
+							<li className="active"><a href="#">Page 1</a></li>
+							<li><a href="#">Page 2</a></li>
+						</ul>
+						<ul className="nav navbar-nav navbar-right">
+							<li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+							<li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		);
+	}
+}
+
+export default NavBar;
